b3b26750a344cfd3b6c4ddc5bf3d914abf623331
Anna <annachristinethomas@gmail.com>
2024-09-13 22:52:58 -0700

    Add watch flag to ts files

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,6 @@ const port = 80;
 // Serve static directory
 app.use(express.static("static"));
 
-app.get("/api/oath", (req, res) => {});
+app.get("/api/oath", (_req, _res) => {});
 
 app.listen(port);
